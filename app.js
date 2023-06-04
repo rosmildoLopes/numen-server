b41edb3278670bb0679bb9c363e98063527350e6
rosmildoLopes <[200~123316625+rosmildoLopes@users.noreply.github.com~>
2023-06-04 13:11:10 -0300

    Routes and Controllers

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json)
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const indexRouter = require('./routes/index')
 
+app.use('/', indexRouter)
 
 module.exports = app ;
