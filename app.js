ec476b24e092f34e61c232ffe10c6ad0f515b8ba
rosmildoLopes <[200~123316625+rosmildoLopes@users.noreply.github.com~>
2023-06-09 00:28:19 -0300

    env and momgoose

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const app = express();
 const logger = require("morgan");
+const cors = require('cors');
 
 app.use(express.json());
 app.use(logger("dev"));
+app.use(cors())
 
 const indexRouter = require("./routes/index");
 const apiRouter = require("./routes/api");
+const {connect} =require('./db/db')
 
 app.use("/", indexRouter);
 app.use("/api/v1", apiRouter);
-
-// POST route
-app.post("/api/v1/crear", apiController.crear);
+connect()
 
 module.exports = app;
